664923474b35a76c03a890b16313113e98bc5bed
rajdchak <rajdchak@amazon.co.uk>
2025-01-06 17:21:42 +0000

    Format fix

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AAuditHeaders.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AAuditHeaders.java
@@ -6,15 +6,15 @@ import software.amazon.s3.analyticsaccelerator.request.AuditHeaders;
 
 public class S3AAuditHeaders implements AuditHeaders {
 
-
     HttpReferrerAuditHeader referrer;
+
     public S3AAuditHeaders(HttpReferrerAuditHeader referrer) {
         this.referrer = referrer;
     }
+
     @Override
     public void setGetRange(String range) {
         referrer.set(AuditConstants.PARAM_RANGE, range);
-
     }
 
     @Override
