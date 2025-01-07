2bc740df6a6635214a03ac470f58bf3e727ed73b
rajdchak <rajdchak@amazon.co.uk>
2025-01-07 23:28:47 +0000

    Made interface generic

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AAuditHeaders.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AAuditHeaders.java
@@ -3,6 +3,7 @@ package org.apache.hadoop.fs.s3a;
 import org.apache.hadoop.fs.audit.AuditConstants;
 import org.apache.hadoop.fs.store.audit.HttpReferrerAuditHeader;
 import software.amazon.s3.analyticsaccelerator.request.AuditHeaders;
+import software.amazon.s3.analyticsaccelerator.request.GetRequest;
 
 import static org.apache.hadoop.fs.audit.AuditConstants.PARAM_THREAD0;
 import static org.apache.hadoop.fs.audit.AuditConstants.PARAM_TIMESTAMP;
@@ -17,9 +18,9 @@ public class S3AAuditHeaders implements AuditHeaders {
     }
 
     @Override
-    public String modifyAndBuildReferrerHeader(String range) {
+    public String modifyAndBuildReferrerHeader(GetRequest getRequestContext) {
         HttpReferrerAuditHeader copyReferrer = new HttpReferrerAuditHeader(this.referrer);
-        copyReferrer.set(AuditConstants.PARAM_RANGE, range);
+        copyReferrer.set(AuditConstants.PARAM_RANGE, getRequestContext.getRange().toHttpString());
         return copyReferrer.buildHttpReferrer();
-    }
+    } 
 }
